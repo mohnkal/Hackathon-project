292a8f9b4af3e54f3585538cc93d66b112aca031
Mohnish <mohnishkalaimani@gmail.com>
2023-12-14 03:32:01 +0530

    Job Page Done

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route path="/" element={<LandingPage/>} />
           <Route path="/signup" element={<Signup/>} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/jobs" element={<JobListing/>} />
         </Routes>
         <Footer />
     </BrowserRouter>
