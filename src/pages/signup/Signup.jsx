e0cbce6c00ce466eadbf8d275b7a95fde114939f
anjaanid <sujj4646@gmail.com>
2023-12-13 17:06:11 +0530

    changes by sujal

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import "./style.scss";
 
 const Signup = () => {
-  return <div>
+  return <div className="mainDiv">
+    <div className="textDetailsDiv">
     <h1>Create a new user</h1>
+    <p className="description">It looks like you're new here. We need a bit more info to create your new account. Already have an
+account? <a href="/login" className="blueText"> Login to get started.</a></p>
+    </div>
+    <div className="inputBoxes">
+      
+    </div>
   </div>;
 };
 
