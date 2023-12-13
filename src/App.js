5d4848a14552cc38fc3651b1ed08cc54668e1630
anjaanid <sujj4646@gmail.com>
2023-12-13 16:18:04 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+        <Footer />
+    </BrowserRouter>
   );
 }
 
